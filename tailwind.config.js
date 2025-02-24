64078a3ba4f42633e34a022f5a1c463e6efbfd08
khushboo-barnoliya <khushboobarnolia83423@gmail.com>
2025-02-24 22:58:06 +0530

    second upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // if your files are under `src` folder
+    "./src/**/*.{js,ts,jsx,tsx}", // If your files are under the `src` folder
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
